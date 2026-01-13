3555d31732d8629785e9e20f3c226c3e51f119ef
Abhi10455 <abhinayj729@gmail.com>
2026-01-13 18:09:28 +0530

    Delete Day1WiproTraining directory

--- Day1WiproTraining/Lab/Question2.py
+++
@@ -1,8 +0,0 @@
-data = [1, 2, 3, 4, 5, 6, 2, 4]
-squareslist = [a**2 for a in data]
-uniqueevenset = {a for a in data if a % 2 == 0}
-cubedict = {a: a**3 for a in data}
-
-print("Squares List:", squareslist)
-print("Unique Even Set:", uniqueevenset)
-print("Cube Dictionary:", cubedict)
